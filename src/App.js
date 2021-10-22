96d5e8e87497522a5ab82b4df2ad8e7bd0849953
Gugan A R <guganriz101@gmail.com>
2021-10-22 13:47:40 +0530

    router commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/*----------Importing Navbar.js----------*/}
+      <Navbar />
+
+      {/*----------Importing Home.js----------*/}
+      <Route path="/" exact>
+        <Home />
+      </Route>
+
+      {/*----------Importing Contact.js----------*/}
+      <Route path="/contact">
+        <Contact />
+      </Route>
+
+      {/*----------Importing About.js----------*/}
+      <Route path="/about">
+        <About />
+      </Route>
+    </Router>
   );
 }
 
